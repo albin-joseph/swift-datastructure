e632dd1367844ba32cd2fd96004d7a9f7e12f80b
albin-joseph <albinjoseph007@gmail.com>
2021-10-07 10:26:20 +0530

    implement delete from an index

--- Swift_dataStaructure/Swift_dataStaructure/DSArray.swift
+++ Swift_dataStaructure/Swift_dataStaructure/DSArray.swift
@@ -46,11 +46,26 @@ struct AJArray{
     }
     
     //Pop an item from an array
-    mutating func pop()->String?{
+    @discardableResult mutating func pop()->String?{
         guard let element = data[length-1] else { return nil }
         self.length -= 1
         data.removeValue(forKey: length)
         return element
+    }
+    
+    //Delete an item at index
+    
+    @discardableResult mutating func delete(_ index:Int)->String?{
+        guard let itemToDelete = data[index] else { return nil }
+        shiftIndices(index)
+        return itemToDelete
+    }
+    
+     mutating func shiftIndices(_ fromIndex:Int)->(){
+        for i in fromIndex ..< length-1{
+            data[i] = data[i+1]
+        }
+         self.pop()
     }
 }
 
@@ -63,6 +78,10 @@ func arraActions(){
     print(array.getItem(1))
     array.push("Emmanuel")
     print(array.pop() ?? "empty")
+    print(array)
+    array.push("Jose")
+    array.push("Lissy")
+    array.delete(1)
     print(array)
 }
 
