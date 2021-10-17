3826c5b36ff66a30fab6b4e73a99fb1723533e00
albin-joseph <albinjoseph007@gmail.com>
2021-10-17 08:52:39 +0530

    add stack implementation

---
+++ Swift_dataStaructure/Swift_dataStaructure/StackLinkedList.swift
@@ -0,0 +1,22 @@
+//
+//  StackLinkedList.swift
+//  Swift_dataStaructure
+//
+//  Created by Albin Joseph on 17/10/21.
+//
+
+import Foundation
+
+class Stack{
+    var top:Node?
+    var bottom:Node?
+    var length:Int = 0
+    
+    func push(_ value:String) -> () {
+        
+    }
+    
+    func pop() -> String {
+        return ""
+    }
+}
